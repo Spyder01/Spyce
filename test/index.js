f0fcce689ce7bfa62cc73c546e9a8862b4d1530f
spyder01 <suhan01.bangera@gmail.com>
2021-06-20 00:51:35 +0530

    Implemented Typescript

--- test/index.js
+++ test/index.js
@@ -1,6 +1,9 @@
 const Spyce = require('../index');
 
 
+
+
+
 //Initializing Spyce App
 const app = new Spyce();
 
@@ -12,17 +15,59 @@ const api1 = app.createApi('/api', ()=>{
 api1.request({
     GET: (req, res) => {
         res.setHeader('Content-Type', 'text/*')
-        res.write('Get Request')
-        console.log("Get Request Made")
+        res.end('Get Request')
+    
+       for(let i=0; i<1000; i++){
+        if(i%10==0)
+        console.log(i)
+       } 
     },
     POST: (req, res) => {
         res.setHeader('Content-Type', 'text/html')
-        res.write('Post Request')
+        res.end('Post Request')
         console.log("Post Request Made")
+    },
+    PUT: (req, res) => {
+        res.setHeader('Content-Type', 'text/html')
+        res.end('Put Request')
+        console.log("Put Request Made")
     }
 })
 
 app.listen(5000, ()=>{
-    console.log("Server Started")
+    console.log("Spyce Server Started...")
+})
+
+
+//testing speed with express
+const express = require('express');
+const app2 = express()
+
+
+app2.get('/api', (req, res) => {
+    res.setHeader('Content-Type', 'text/*')
+    res.end('Get Request')
+
+    for(let i=0; i<1000; i++){
+        if(i%10==0)
+        console.log(i)
+       } 
+})
+
+app2.post('/api', (req, res) => {
+    res.setHeader('Content-Type', 'text/html')
+    res.end('Post Request')
+    console.log("Post Request Made")
+})
+
+app2.put('/api', (req, res) => {
+    res.setHeader('Content-Type', 'text/html')
+    res.end('Put Request')
+    console.log("Put Request Made")
+})
+
+
+app2.listen(5001, ()=>{
+    console.log("Express Server Started...")
 })
 
