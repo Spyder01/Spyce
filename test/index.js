0350aae48059d0d7fc41bf1dda57e33c61c15f21
spyder01 <suhan01.bangera@gmail.com>
2021-06-20 01:03:45 +0530

    Implemented Typescript

--- test/index.js
+++ test/index.js
@@ -39,35 +39,3 @@ app.listen(5000, ()=>{
 })
 
 
-//testing speed with express
-const express = require('express');
-const app2 = express()
-
-
-app2.get('/api', (req, res) => {
-    res.setHeader('Content-Type', 'text/*')
-    res.end('Get Request')
-
-    for(let i=0; i<1000; i++){
-        if(i%10==0)
-        console.log(i)
-       } 
-})
-
-app2.post('/api', (req, res) => {
-    res.setHeader('Content-Type', 'text/html')
-    res.end('Post Request')
-    console.log("Post Request Made")
-})
-
-app2.put('/api', (req, res) => {
-    res.setHeader('Content-Type', 'text/html')
-    res.end('Put Request')
-    console.log("Put Request Made")
-})
-
-
-app2.listen(5001, ()=>{
-    console.log("Express Server Started...")
-})
-
